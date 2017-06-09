3ff1bd8a65708691ab49a4373883e898db762c83
Cameron Kearney <ckearney@ksmpartners.com>
2017-06-09 14:15:40 -0400

    all files contain comments

--- app/utils/Conversions.js
+++ app/utils/Conversions.js
@@ -1,3 +1,4 @@
+//conversion formulae and tables to turn unix dates to user readable dates.
 var daysMap = {
   "0":"Sunday",
   "1":"Monday",
@@ -22,11 +23,11 @@ var monthsMap = {
   "10":"Nov",
   "11":"Dec",
 };
-
+//converts temperatures recieved in K to F
 function convertTemp(kelvin) {
   return parseInt(((kelvin - 273.15)* 1.8 + 32.00), 10)
 }
-
+//converts the unix date into a day and month then append them together
 function getDate(unixTimestamp) {
   var date = new Date(unixTimestamp * 1000);
   var day = daysMap[date.getDay()]
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+//calls out to the Open Weather API
 var _baseURL = 'http://api.openweathermap.org/data/2.5/';
 var _APIKEY = '61c9fba8c579c55b05af1644f5297a8d';
-
+//set up the data to be included in the request
 function getQueryStringData (city, days) {
   return {
     q: city,
@@ -11,18 +11,18 @@ function getQueryStringData (city, days) {
     cnt: days
   }
 }
-
+//format the paramiters by mapping over them to append into an encoded string
 function prepRouteParams (queryStringData) {
   return Object.keys(queryStringData)
   .map((key) => {
     return key + '=' + encodeURIComponent(queryStringData[key]);
   }).join('&')
 }
-
+//append the routeparams onto the basic url
 function prepUrl (type, queryStringData) {
   return _baseURL + type + '?' + prepRouteParams(queryStringData);
 }
-
+//create a callout url for a single day forecast
 function getCurrentWeather (city) {
   var queryStringData = getQueryStringData(city);
   var url = prepUrl('weather', queryStringData)
@@ -32,7 +32,7 @@ function getCurrentWeather (city) {
       return currentWeatherData.data
     })
 }
-
+//create a callout url for multiple days
 function getForcast (city, days) {
   var queryStringData = getQueryStringData(city, days);
   var url = prepUrl('forecast/daily', queryStringData)
